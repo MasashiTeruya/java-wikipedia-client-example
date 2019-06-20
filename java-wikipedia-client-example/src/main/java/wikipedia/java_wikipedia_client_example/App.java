18808bc70aeed130080b98a58e82c55189cc3a34
Masashi Teruya <MasashiTeruya@outlook.com>
2019-06-21 02:36:32 +0900

    Implements a client.

---
+++ java-wikipedia-client-example/src/main/java/wikipedia/java_wikipedia_client_example/App.java
@@ -0,0 +1,38 @@
+package wikipedia.java_wikipedia_client_example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) throws SQLException {
+		String databaseHost = System.getenv("WIKIPEDIA_DB_HOST");
+		String username = System.getenv("WIKIPEDIA_DB_USERNAME");
+		String password = System.getenv("WIKIPEDIA_DB_PASSWORD");
+		String databaseName = System.getenv("WIKIPEDIA_DB_NAME");
+		String url = String.format("jdbc:mysql://%s/%s", databaseHost, databaseName);
+		try (Connection con = DriverManager.getConnection(url, username, password)) {
+			Integer id = 99;
+			String title = getPageTitleByPageID(con, id);
+			System.out.println(title);
+		}
+	}
+
+	public static String getPageTitleByPageID(Connection con, Integer id) throws SQLException {
+		String sql = "SELECT page_title FROM page WHERE page_id = ?;";
+		PreparedStatement preparedStatement = con.prepareStatement(sql);
+		preparedStatement.setInt(1, id);
+		ResultSet resultSet = preparedStatement.executeQuery();
+		while (resultSet.next()) {
+			String title = new String(resultSet.getBytes("page_title"));
+			return title;
+		}
+		return "";
+	}
+}
